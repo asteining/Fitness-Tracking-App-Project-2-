84b5f18df51bb1b1ae19c05c7b9a2979ff1f0017
Lacy Ann Taylor <lacy.ann.taylor@gmail.com>
2025-01-21 20:47:24 -0600

    testing commit

---
+++ server/src/models/exerciselog.ts
@@ -0,0 +1,65 @@
+import { DataTypes, type Sequelize, Model, type Optional } from 'sequelize';
+import bcrypt from 'bcrypt';
+
+interface ExerciseAttributes {
+  id: number;
+  exerciseType: string;
+  caloriesBurned: string;
+  hoursPerformed: string;
+}
+
+interface ExerciseCreationAttributes extends Optional<ExerciseAttributes, 'id'> {}
+
+export class Exercise
+  extends Model<ExerciseAttributes, ExerciseCreationAttributes>
+  implements ExerciseAttributes
+{
+  public id!: number;
+  public exerciseType!: string;
+  public caloriesBurned!: string;
+  public hoursPerformed!: string;
+
+
+  public readonly createdAt!: Date;
+  public readonly updatedAt!: Date;
+
+
+}
+
+export function UserFactory(sequelize: Sequelize): typeof Exercise {
+  Exercise.init(
+    {
+      id: {
+        type: DataTypes.INTEGER,
+        autoIncrement: true,
+        primaryKey: true,
+      },
+      exerciseType: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      caloriesBurned: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      hoursPerformed: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+    },
+    {
+      tableName: 'Exercise',
+      sequelize,
+      //hooks: {
+       // beforeCreate: async (user: User) => {
+       //   await user.setPassword(user.password);
+       // },
+       // beforeUpdate: async (user: User) => {
+       //   await user.setPassword(user.password);
+       // },
+      //},
+    }
+  );
+
+  return Exercise;
+}
