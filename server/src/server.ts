7a7d2d6ba84f703a38c65f0db3bf424227431a38
Aramis Steininger <aramissteininger@gmail.com>
2025-01-24 20:55:36 -0500

    server-file-edits

--- server/src/server.ts
+++ server/src/server.ts
@@ -7,20 +7,6 @@ import routes from './routes/index.js';
 const app = express();
 const PORT = process.env.PORT || 3001;
 
-// Middleware to log incoming requests and headers
-app.use((req, _res, next) => {
-  console.log(`Incoming Request: ${req.method} ${req.url}`);
-  console.log('Headers:', req.headers);
-
-  // Specifically log the X-Api-Key header
-  if (req.headers['x-api-key']) {
-    console.log('X-Api-Key:', req.headers['x-api-key']);
-  } else {
-    console.warn('X-Api-Key header is missing!');
-  }
-
-  next(); // Continue to the next middleware or route
-});
 
 // Serves static files in the entire client's dist folder
 app.use(express.static('../client/dist'));
